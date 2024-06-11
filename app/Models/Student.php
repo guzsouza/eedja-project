eae63aa930cb3f86e712a9ddefbeb1e988d21938
guzsouza <125076268+guzsouza@users.noreply.github.com>
2024-06-11 20:04:43 -0300

    first commit

---
+++ app/Models/Student.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model{
+    use HasFactory;
+    protected $table = 'students';
+
+    protected $fillable = [
+        'name'
+    ];
+    
+    protected $hidden = [
+
+    ];
+    
+    protected $casts = [
+
+    ];
+
+    //Relações Eloquent
+    /*
+        Daq pra baixo relações eloquent
+    */
+}
