5b0fa2845c67156c6e00fa010d9d399a43908dbb
raul <raulntjj@gmail.com>
2024-06-24 15:34:36 -0300

    a

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,6 +7,11 @@
             <li><a href="#">Serviços</a></li>
             <li><a href="#">Portfólio</a></li>
             <li><a href="#">Contato</a></li>
+            <li>
+              <form action="{{ route('logout') }}" method="POST">
+                <button type="submit">Sair</button>
+              </form>
+            </li>
           @endauth
           @guest
             <li><a href="#login" class="cta">Entrar</a></li>
