66d773f3531e64bc1d61e6495ad916dbd850a73d
raul <raulntjj@gmail.com>
2024-06-24 14:21:23 -0300

    a

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,14 @@
 <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="align-text-top d-inline-block">
-    </a>
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="align-text-top d-inline-block">
+        </a>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <p>Sair</p>
+    </div>
   </div>
 </nav>
